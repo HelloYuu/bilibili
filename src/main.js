d7053c88739bf9f282e32040c1294152340e17ef
wang <734455123@qq.com>
2020-01-06 19:20:18 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './utils/register-vant.js' // 注册 Vant 组件
+import 'amfe-flexible' // 配置 REM 适配
 
 Vue.config.productionTip = false
 
